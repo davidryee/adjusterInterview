7b3859fa7fe8e1abbbfda4372bb73d8a30ea07e7
David Yee <davidryee@gmail.com>
2017-11-29 00:02:08 -0800

    Completed clicks and views counts

---
+++ src/homework/httpclient/ClicksTextToCsvResults.java
@@ -0,0 +1,28 @@
+package homework.httpclient;
+
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import java.util.Scanner;
+
+public class ClicksTextToCsvResults {
+
+	public static void main(String[] args) throws Exception {
+	    final Path path = Paths.get("C:\\DBProof");
+	    final Path txt = path.resolve("TotalClicks.txt");
+	    final Path csv = path.resolve("TotalClicks.csv");
+	    final Charset utf8 = Charset.forName("UTF-8");
+	    try (
+	             Scanner scanner = new Scanner(Files.newBufferedReader(txt, utf8));
+	            final PrintWriter pw = new PrintWriter(Files.newBufferedWriter(csv, utf8, StandardOpenOption.CREATE_NEW))) {
+	        while (scanner.hasNextLine()) {
+	            pw.println(scanner.nextLine().replace('\t', ','));
+	        }
+
+	    }
+	}
+}
